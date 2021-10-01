746949b8ec5612e9562f4292a0e62b5f56554f2c
richard.acevedo <richardacevedo98@gmail.com>
2021-10-01 09:47:46 -0500

    cambios

--- Jenkinsfile
+++ Jenkinsfile
@@ -39,6 +39,13 @@ pipeline {
         }
     }
 
+    stage('Construir Proyecto') {
+        steps {
+          echo '------------>Building<------------'
+          sh 'npm run build'
+        }
+    }
+
     stage('Tests') {
         steps {
             sh 'npm test'
@@ -55,13 +62,6 @@ pipeline {
     }
 
 
-    stage('Build') {
-            steps {
-              echo '------------>Building<------------'
-              sh 'npm run build'
-            }
-        }
-  }
   post {
     always {
       echo 'This will always run'
--- src/app/feature/usuario/components/usario-home/usario-home.component.spec.ts
+++ src/app/feature/usuario/components/usario-home/usario-home.component.spec.ts
@@ -1,25 +1,3 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { UsarioHomeComponent } from './usario-home.component';
-
 describe('UsarioHomeComponent', () => {
-  let component: UsarioHomeComponent;
-  let fixture: ComponentFixture<UsarioHomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UsarioHomeComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UsarioHomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
